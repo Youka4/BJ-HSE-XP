1b2e3516f1ede26e1b725c0f9f6a71cf5c79063e
Youka4 <dim.youka4@gmail.com>
2024-10-28 14:39:49 +0300

    bug fixed

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 
 import tkinter as tk
 from gui.game_screen import GameScreen
+from gui.gui_manager import GUIManager
+
 
 def main():
     root = tk.Tk()
     root.geometry("600x400")
-    GameScreen(root, lambda: root.quit())
+    GUIManager(root)
     root.mainloop()
 
 if __name__ == "__main__":
